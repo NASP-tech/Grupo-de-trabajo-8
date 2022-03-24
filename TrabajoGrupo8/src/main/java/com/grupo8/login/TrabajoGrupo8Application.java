534f3407b7e751eb67c3a904ea7fb645ee562943
Natalia Solorzano <00120319@uca.edu.sv>
2022-03-23 19:48:23 -0600

    Add: New Spring Tool Project N-Capas 01-2022

---
+++ TrabajoGrupo8/src/main/java/com/grupo8/login/TrabajoGrupo8Application.java
@@ -0,0 +1,13 @@
+package com.grupo8.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoGrupo8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoGrupo8Application.class, args);
+	}
+
+}
