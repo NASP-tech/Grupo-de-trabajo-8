a5bbf762990a41377637fb90d7fb376766474f09
DanielaZepeda <00062019@uca.edu.sv>
2022-03-24 07:06:15 -0600

    Agregando login.html y loginAdmin.html

---
+++ TrabajoGrupo8/src/main/java/com/grupo8/login/controllers/UserControllers.java
@@ -0,0 +1,16 @@
+package com.grupo8.login.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserControllers {
+	
+	@GetMapping("/ ")
+	public String getMainPageUser() {
+		return "login";
+	}
+ 
+}
